2f93f9a7498c504da99e40c5eda6c91182ab5a5a
Geoff Lankow <geoff@darktrojan.net>
2013-03-13 22:50:58 +1300

    Rewrite preferences dialog

--- chrome/content/config.js
+++ chrome/content/config.js
@@ -7,154 +7,126 @@ Cu.import('resource://gre/modules/Services.jsm');
 const IS_FIREFOX = Services.appinfo.name == 'Firefox';
 const IS_THUNDERBIRD = Services.appinfo.name == 'Thunderbird';
 
-if (!IS_THUNDERBIRD) {
-	var siteList = document.getElementById('sitelist');
+for (let element of document.querySelectorAll('[id]')) {
+	window[element.id] = element;
 }
 
-var maxWidthPref = document.getElementById('maxwidth');
-var maxHeightPref = document.getElementById('maxheight');
-var qualityPref = document.getElementById('quality');
-
-var noresize = document.getElementById('noresize');
-var small = document.getElementById('small');
-var medium = document.getElementById('medium');
-var large = document.getElementById('large');
-var custom = document.getElementById('custom');
-var label1 = document.getElementById('label1');
-var customvalue1 = document.getElementById('customvalue1');
-var label2 = document.getElementById('label2');
-var customvalue2 = document.getElementById('customvalue2');
-var slider = document.getElementById('slider');
-
-var strings = document.getElementById('shrunked-strings');
-
-function doLoad() {
-	let lastSelected = document.documentElement.getAttribute('lastSelected');
-	if (lastSelected) {
-		let lastSelectedPane = document.getElementById(lastSelected);
-		if (lastSelectedPane) {
-			document.documentElement.showPane(lastSelectedPane);
-		}
-	}
-
-	let sitesPane = document.getElementById('shrunked-sites');
-	let defaultsPane = document.getElementById('shrunked-defaults');
-
+function load() {
 	if (IS_FIREFOX) {
-		noresize.collapsed = true;
+		r_noresize.collapsed = true;
 	}
-	if (IS_THUNDERBIRD) {
-		try {
-			document.documentElement.showPane(defaultsPane);
-		} catch (e) {
-			Cu.reportError(e);
-		}
-		sitesPane.setAttribute('collapsed', 'true');
-		var selector = document.getAnonymousElementByAttribute(document.documentElement, 'anonid', 'selector');
-		selector.setAttribute('collapsed', 'true');
+	let maxWidth = p_maxwidth.value;
+	let maxHeight = p_maxheight.value;
+	if (maxWidth == -1 && maxHeight == -1) {
+		rg_size.selectedIndex = 0;
+	} else if (maxWidth == 500 && maxHeight == 500) {
+		rg_size.selectedIndex = 1;
+	} else if (maxWidth == 800 && maxHeight == 800) {
+		rg_size.selectedIndex = 2;
+	} else if (maxWidth == 1200 && maxHeight == 1200) {
+		rg_size.selectedIndex = 3;
 	} else {
-		var data = {};
-		['disabled', 'maxWidth', 'maxHeight'].forEach(function(name) {
+		rg_size.selectedIndex = 4;
+		tb_width.value = maxWidth;
+		tb_height.value = maxHeight;
+	}
+	setSize();
+
+	s_quality.value = p_quality.value;
+
+	if (IS_THUNDERBIRD) {
+		t_sites.hidden = true;
+	} else {
+		let data = {};
+		for (let name of ['disabled', 'maxWidth', 'maxHeight']) {
 			let prefs = Services.contentPrefs.getPrefsByName('extensions.shrunked.' + name, null);
 			let enumerator = prefs.enumerator;
 			while (enumerator.hasMoreElements()) {
-				var property = enumerator.getNext().QueryInterface(Components.interfaces.nsIProperty);
+				let property = enumerator.getNext().QueryInterface(Ci.nsIProperty);
 				if (!(property.name in data)) {
 					data[property.name] = {};
 				}
 				data[property.name][name] = property.value;
 			}
-		});
+		}
 		handleData(data);
 	}
 
-	var maxWidth = maxWidthPref.value;
-	var maxHeight = maxHeightPref.value;
+	enableExif();
 
-	if (maxWidth == -1 && maxHeight == -1) {
-		medium.parentNode.selectedIndex = 0;
-	} else if (maxWidth == 500 && maxHeight == 500) {
-		medium.parentNode.selectedIndex = 1;
-	} else if (maxWidth == 800 && maxHeight == 800) {
-		medium.parentNode.selectedIndex = 2;
-	} else if (maxWidth == 1200 && maxHeight == 1200) {
-		medium.parentNode.selectedIndex = 3;
-	} else {
-		medium.parentNode.selectedIndex = 4;
-		customvalue1.value = maxWidthPref.value;
-		customvalue2.value = maxHeightPref.value;
-	}
-
-	slider.value = qualityPref.value;
-
-	validate();
 	window.sizeToContent();
 }
 
+function setSize() {
+	l_width.disabled = tb_width.disabled =
+		l_height.disabled = tb_height.disabled = !r_custom.selected;
+	if (r_noresize.selected) {
+		p_maxwidth.value = -1;
+		p_maxheight.value = -1;
+	} else if (r_small.selected) {
+		p_maxwidth.value = 500;
+		p_maxheight.value = 500;
+	} else if (r_medium.selected) {
+		p_maxwidth.value = 800;
+		p_maxheight.value = 800;
+	} else if (r_large.selected) {
+		p_maxwidth.value = 1200;
+		p_maxheight.value = 1200;
+	} else {
+		p_maxwidth.value = tb_width.value;
+		p_maxheight.value = tb_height.value;
+	}
+}
+
+function enableExif() {
+	cb_gps.disabled = !cb_exif.checked;
+}
+
 function handleData(data) {
-	for (var site in data) {
-		var disabled = data[site]['disabled'];
-		var maxWidth = data[site]['maxWidth'];
-		var maxHeight = data[site]['maxHeight'];
+	for (let site in data) {
+		let disabled = data[site]['disabled'];
+		let maxWidth = data[site]['maxWidth'];
+		let maxHeight = data[site]['maxHeight'];
 		if (disabled) {
-			var item = document.createElement('listitem');
+			let item = document.createElement('listitem');
 			item.setAttribute('style', 'color: #666; font-style: italic');
-			var siteCell = document.createElement('listcell');
+			let siteCell = document.createElement('listcell');
 			siteCell.setAttribute('label', site);
 			item.appendChild(siteCell);
-			var disabledCell = document.createElement('listcell');
+			let disabledCell = document.createElement('listcell');
 			disabledCell.setAttribute('label', strings.getString('disabled'));
 			disabledCell.setAttribute('style', 'text-align: center');
 			item.appendChild(disabledCell);
-			siteList.appendChild(item);
+			lb_sites.appendChild(item);
 		}
 		if (maxWidth && maxHeight) {
-			var item = document.createElement('listitem');
-			var siteCell = document.createElement('listcell');
+			let item = document.createElement('listitem');
+			let siteCell = document.createElement('listcell');
 			siteCell.setAttribute('label', site);
 			item.appendChild(siteCell);
-			var widthCell = document.createElement('listcell');
+			let widthCell = document.createElement('listcell');
 			widthCell.setAttribute('label', strings.getFormattedString('dimensions', [maxWidth, maxHeight]));
 			widthCell.setAttribute('style', 'text-align: center');
 			item.appendChild(widthCell);
-			siteList.appendChild(item);
+			lb_sites.appendChild(item);
 		}
 	}
 }
 
-function doForget() {
-	var item = siteList.getSelectedItem(0);
+function enableForget() {
+	b_forget.disabled = lb_sites.selectedItem == null;
+}
+
+function forgetSite() {
+	let item = lb_sites.getSelectedItem(0);
 	if (item) {
-		var site = item.firstChild.getAttribute('label');
-		var u = Services.io.newURI('http://' + site + '/', null, null);
+		let site = item.firstChild.getAttribute('label');
+		let uri = Services.io.newURI('http://' + site + '/', null, null);
 
-		Services.contentPrefs.removePref(u, 'extensions.shrunked.maxHeight', null);
-		Services.contentPrefs.removePref(u, 'extensions.shrunked.maxWidth', null);
-		Services.contentPrefs.removePref(u, 'extensions.shrunked.disabled', null);
+		Services.contentPrefs.removePref(uri, 'extensions.shrunked.maxHeight', null);
+		Services.contentPrefs.removePref(uri, 'extensions.shrunked.maxWidth', null);
+		Services.contentPrefs.removePref(uri, 'extensions.shrunked.disabled', null);
+
+		lb_sites.removeChild(item);
 	}
-	siteList.removeChild(item);
-}
-
-function validate() {
-	label1.disabled = customvalue1.disabled = label2.disabled = customvalue2.disabled = !custom.selected;
-	if (noresize.selected) {
-		maxWidthPref.value = -1;
-		maxHeightPref.value = -1;
-	} else if (small.selected) {
-		maxWidthPref.value = 500;
-		maxHeightPref.value = 500;
-	} else if (medium.selected) {
-		maxWidthPref.value = 800;
-		maxHeightPref.value = 800;
-	} else if (large.selected) {
-		maxWidthPref.value = 1200;
-		maxHeightPref.value = 1200;
-	} else {
-		maxWidthPref.value = customvalue1.value;
-		maxHeightPref.value = customvalue2.value;
-	}
-}
-
-function doAccept() {
-	document.documentElement.setAttribute('lastSelected', document.documentElement.currentPane.id);
 }
