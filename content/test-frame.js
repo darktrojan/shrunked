016a01bc7fca56c7a4a879b1e37d604f947e4dc2
Geoff Lankow <geoff@darktrojan.net>
2015-05-15 14:55:15 +1200

    Fix mistakes from chrome/ move

--- content/test-frame.js
+++
@@ -1,8 +0,0 @@
-addMessageListener("setfiles", function(message) {
-	let myChromeFile = message.data;
-	console.log(myChromeFile);
-
-	var contentExposedFile = Components.utils.cloneInto(myChromeFile, content);
-
-	content.document.querySelector('input[type="file"]').mozSetFileArray([contentExposedFile]);
-});
